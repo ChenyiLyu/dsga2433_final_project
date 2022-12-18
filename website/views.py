1250c7288b89f03c551ebe789e07036923968ab6
Chenyi Lyu <chenyi@HeiYanQuan-1094.local>
2022-12-18 19:42:23 +0800

    done

--- website/views.py
+++ website/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
 
+
 def home(request):
     return render(request, 'home.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+
+def service(request):
+    return render(request, 'service.html', {})
+
+
+def pricing(request):
+    return render(request, 'pricing.html', {})
 
 
 def contact(request):
